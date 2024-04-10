843b2cce0ddc3243a93a4b5615a94681fd9b6b89
Mercedes Librero <mercedeslibrero@gmail.com>
2024-04-10 12:11:13 +0200

    interface IMuestra class MuestraHTMLAmericano implements IMuestra class MuestraHTMLEuropeo implements IMuestra

--- ModuloLunarGruboB/wwwroot/js/main.js
+++ ModuloLunarGruboB/wwwroot/js/main.js
@@ -14,4 +14,20 @@ var Mineral = /** @class */ (function () {
     }
     return Mineral;
 }());
+var MuestraHTMLAmericano = /** @class */ (function () {
+    function MuestraHTMLAmericano() {
+    }
+    MuestraHTMLAmericano.prototype.dameContenido = function (MiMineral) {
+        return ("<p>Identifier: ".concat(MiMineral.idMineral, " </p> <br/>\n                 <p>Name: ").concat(MiMineral.nombre, " </p> <br/>\n                 <p>Group/Origin: ").concat(MiMineral.grupo_origen, " </p> <br/>\n                 <p>Hardness: ").concat(MiMineral.dureza, " </p> <br/>\n                 <p>Grain Size: ").concat(MiMineral.dureza, " </p> <br/>\n                 <p>Sort: ").concat(MiMineral.clasificacion, " </p> <br/>\n                 <p>Crystal Size: ").concat(MiMineral.tam_cristales, " </p> <br/> \n                 <p>Formation Temperature: ").concat(((MiMineral.temp_formacion - 273.15) * 9) / 5 + 32, " \u00B0F </p> <br/> \n                 <p>Structure: ").concat(MiMineral.estructura, " </p> <br/> \n                 <p>Forms Grains: ").concat(MiMineral.forma_granos, " </p> <br/> \n                 <p>Texture: ").concat(MiMineral.textura, " </p> <br/>   "));
+    };
+    return MuestraHTMLAmericano;
+}());
+var MuestraHTMLEuropeo = /** @class */ (function () {
+    function MuestraHTMLEuropeo() {
+    }
+    MuestraHTMLEuropeo.prototype.dameContenido = function (MiMineral) {
+        return ("<p>Identificador: ".concat(MiMineral.idMineral, " </p> <br/>\n                 <p>Nombre: ").concat(MiMineral.nombre, " </p> <br/>\n                 <p>Grupo/Origen: ").concat(MiMineral.grupo_origen, " </p> <br/>\n                 <p>Dureza: ").concat(MiMineral.dureza, " </p> <br/>\n                 <p>Tama\u00F1o Grano: ").concat(MiMineral.tam_grano, " mm </p> <br/>\n                 <p>Clasificaci\u00F3n: ").concat(MiMineral.clasificacion, " </p> <br/>\n                 <p>Tama\u00F1o Cristales: ").concat(MiMineral.tam_cristales, " </p> <br/> \n                 <p>Temperatura Formacion: ").concat((MiMineral.temp_formacion - 273.15), " \u00B0C </p> <br/> \n                 <p>Estructura: ").concat(MiMineral.estructura, " </p> <br/> \n                 <p>Forma Granos: ").concat(MiMineral.forma_granos, " </p> <br/> \n                 <p>Textura: ").concat(MiMineral.textura, " </p> <br/> "));
+    };
+    return MuestraHTMLEuropeo;
+}());
 //# sourceMappingURL=main.js.map
--- ModuloLunarGruboB/wwwroot/js/main.ts
+++ ModuloLunarGruboB/wwwroot/js/main.ts
@@ -11,3 +11,62 @@
     forma_granos: string = "";
     textura: string = "";
 }
+
+//interface IMuestra {
+//    dameContenido(MiPersona: Persona): String;
+//}
+//class MuestraHTML implements IMuestra {
+//    dameContenido(MiPersona: Persona): String {
+//        return (`<p> ${MiPersona.primerNombre}  ${MiPersona.apellido1} </p>`);
+//    }
+//}
+//class MuestraHTML2 implements IMuestra {
+//    dameContenido(MiPersona: Persona): String {
+//        return (`<p> ${MiPersona.primerNombre}  ${MiPersona.apellido2} </p>`);
+//    }
+//}
+//class MuestraHTMLGrupoB implements IMuestra {
+//    dameContenido(MiPersona: Persona): String {
+//        return `Nombre: ${MiPersona.primerNombre} <br/>
+//        Apellido: ${MiPersona.apellido1} <br/>
+//        Año de Nacimiento: ${MiPersona.añoNacimiento.toString()}
+//        ID: ${MiPersona.identificador}`;
+//    }
+
+//}
+
+interface IMuestra {
+    dameContenido(MiMineral: Mineral): String;
+}
+
+class MuestraHTMLAmericano implements IMuestra {
+    dameContenido(MiMineral: Mineral): String {
+        return (`<p>Identifier: ${MiMineral.idMineral} </p> <br/>
+                 <p>Name: ${MiMineral.nombre} </p> <br/>
+                 <p>Group/Origin: ${MiMineral.grupo_origen} </p> <br/>
+                 <p>Hardness: ${MiMineral.dureza} </p> <br/>
+                 <p>Grain Size: ${MiMineral.dureza} </p> <br/>
+                 <p>Sort: ${MiMineral.clasificacion} </p> <br/>
+                 <p>Crystal Size: ${MiMineral.tam_cristales} </p> <br/> 
+                 <p>Formation Temperature: ${ ((MiMineral.temp_formacion - 273.15) * 9) / 5 + 32} °F </p> <br/> 
+                 <p>Structure: ${MiMineral.estructura} </p> <br/> 
+                 <p>Forms Grains: ${MiMineral.forma_granos} </p> <br/> 
+                 <p>Texture: ${MiMineral.textura} </p> <br/>   `);
+    }
+}
+
+class MuestraHTMLEuropeo implements IMuestra {
+    dameContenido(MiMineral: Mineral): String {
+        return (`<p>Identificador: ${MiMineral.idMineral} </p> <br/>
+                 <p>Nombre: ${MiMineral.nombre} </p> <br/>
+                 <p>Grupo/Origen: ${MiMineral.grupo_origen} </p> <br/>
+                 <p>Dureza: ${MiMineral.dureza} </p> <br/>
+                 <p>Tamaño Grano: ${MiMineral.tam_grano} mm </p> <br/>
+                 <p>Clasificación: ${MiMineral.clasificacion} </p> <br/>
+                 <p>Tamaño Cristales: ${MiMineral.tam_cristales} </p> <br/> 
+                 <p>Temperatura Formacion: ${ (MiMineral.temp_formacion - 273.15)} °C </p> <br/> 
+                 <p>Estructura: ${MiMineral.estructura} </p> <br/> 
+                 <p>Forma Granos: ${MiMineral.forma_granos} </p> <br/> 
+                 <p>Textura: ${MiMineral.textura} </p> <br/> `);
+    }
+}
